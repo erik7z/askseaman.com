91b8f604b6519e2ce3ae09ee0b3311c011112812
erik7z <nguita.erik@gmail.com>
2021-02-18 14:38:41 +0200

    using graphqljsonobject to return ranks definitions

--- api/src/graphql/resolvers/type-resolvers.ts
+++ api/src/graphql/resolvers/type-resolvers.ts
@@ -1,10 +1,17 @@
 import { Resolvers } from '../../types/generated-backend'
 import { ApolloServerContext } from '../../types/backend'
+import GraphQLJSON, { GraphQLJSONObject } from 'graphql-type-json'
 
 import { ranks } from './../../data/ranks'
 
 const typeResolvers: Resolvers<ApolloServerContext> = {
-	UserRank: ranks,
+	Query: {
+		UserRanks() {
+			return ranks
+		},
+	},
+	JSON: GraphQLJSON,
+	JSONObject: GraphQLJSONObject,
 }
 
 export default typeResolvers
