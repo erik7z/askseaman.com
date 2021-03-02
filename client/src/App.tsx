6747f8edd128dbe810ee890a901694d01ef02115
erik7z <nguita.erik@gmail.com>
2021-03-02 22:55:12 +0200

    refactored app component to use custom hook

---
+++ client/src/App.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import AuthLayout from './components/Layout/Auth'
+import MainLayout from './components/Layout'
+import { Auth, Me, NotFound, Questions, Tags, Users } from './sections'
+
+import { Tag } from './sections/Tags/Tag'
+import { User } from './sections/Users/User'
+import { Question } from './sections/Questions/Question'
+import { AskQuestion } from './sections/Questions/AskQuestion'
+import { Notifications } from './sections/Me/Notifications'
+import { Register } from './sections/Auth/Register'
+import { useAuthCurrentUser } from './lib/hooks/'
+
+export const App = () => {
+	useAuthCurrentUser()
+
+	return (
+		<Router>
+			<Switch>
+				<Route path={['/auth', '/register']}>
+					<AuthLayout>
+						<Switch>
+							<Route exact path='/auth' component={Auth} />
+							<Route exact path='/register' component={Register} />
+						</Switch>
+					</AuthLayout>
+				</Route>
+				<Route
+					path={[
+						'/',
+						'/questions',
+						'/question',
+						'/ask',
+						'/user',
+						'/me',
+						'/me/notifications',
+						'/tags',
+						'/tag',
+					]}
+				>
+					<MainLayout>
+						<Switch>
+							<Route exact path='/'>
+								<Questions />
+							</Route>
+							<Route path='/question/:questionId'>
+								<Question />
+							</Route>
+							<Route exact path='/ask' component={AskQuestion} />
+							<Route exact path='/user'>
+								<User />
+							</Route>
+							<Route exact path='/me'>
+								<Me />
+							</Route>
+							<Route exact path='/me/notifications'>
+								<Notifications />
+							</Route>
+							<Route exact path='/tags'>
+								<Tags />
+							</Route>
+							<Route exact path='/tag'>
+								<Tag />
+							</Route>
+							<Route exact path='/users'>
+								<Users />
+							</Route>
+
+							<Route>
+								<NotFound />
+							</Route>
+						</Switch>
+					</MainLayout>
+				</Route>
+			</Switch>
+		</Router>
+	)
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import {
 	ApolloClient,
@@ -9,25 +9,13 @@ import {
 	ApolloProvider,
 } from '@apollo/client'
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles.css'
 
-import { CurrentUserProvider, CurrentUserContext } from './lib/contexts'
-import { TOKEN_FIELD, HOST_URI } from './env'
-import AuthLayout from './components/Layout/Auth'
-import MainLayout from './components/Layout'
-import { Auth, Me, NotFound, Questions, Tags, Users } from './sections'
+import { App } from './App'
 
-import { Tag } from './sections/Tags/Tag'
-import { User } from './sections/Users/User'
-import { Question } from './sections/Questions/Question'
-import { AskQuestion } from './sections/Questions/AskQuestion'
-import { Notifications } from './sections/Me/Notifications'
-import { Register } from './sections/Auth/Register'
-
-import { useCurrentUserLazyQuery } from './types/generated-frontend'
+import { CurrentUserProvider } from './lib/contexts'
+import { HOST_URI } from './env'
 
 import reportWebVitals from './reportWebVitals'
 
@@ -57,88 +45,6 @@ const client = new ApolloClient({
 	}),
 	link: concat(authMiddleware, httpLink),
 })
-
-export const App = () => {
-	const [userContext, userDispatch] = useContext(CurrentUserContext)
-
-	const [
-		getCurrentUser,
-		{ data: currentUserResponse },
-	] = useCurrentUserLazyQuery({ fetchPolicy: 'cache-and-network' })
-
-	useEffect(() => {
-		getCurrentUser()
-		console.log('auth useffect')
-		if (currentUserResponse && localStorage.getItem(TOKEN_FIELD)) {
-			userDispatch({
-				type: 'SIGN_IN',
-				payload: currentUserResponse,
-			})
-		}
-	}, [userContext.token, currentUserResponse, userDispatch, getCurrentUser])
-
-	return (
-		<Router>
-			<Switch>
-				<Route path={['/auth', '/register']}>
-					<AuthLayout>
-						<Switch>
-							<Route exact path='/auth' component={Auth} />
-							<Route exact path='/register' component={Register} />
-						</Switch>
-					</AuthLayout>
-				</Route>
-				<Route
-					path={[
-						'/',
-						'/questions',
-						'/question',
-						'/ask',
-						'/user',
-						'/me',
-						'/me/notifications',
-						'/tags',
-						'/tag',
-					]}
-				>
-					<MainLayout>
-						<Switch>
-							<Route exact path='/'>
-								<Questions />
-							</Route>
-							<Route path='/question/:questionId'>
-								<Question />
-							</Route>
-							<Route exact path='/ask' component={AskQuestion} />
-							<Route exact path='/user'>
-								<User />
-							</Route>
-							<Route exact path='/me'>
-								<Me />
-							</Route>
-							<Route exact path='/me/notifications'>
-								<Notifications />
-							</Route>
-							<Route exact path='/tags'>
-								<Tags />
-							</Route>
-							<Route exact path='/tag'>
-								<Tag />
-							</Route>
-							<Route exact path='/users'>
-								<Users />
-							</Route>
-
-							<Route>
-								<NotFound />
-							</Route>
-						</Switch>
-					</MainLayout>
-				</Route>
-			</Switch>
-		</Router>
-	)
-}
 
 ReactDOM.render(
 	// <React.StrictMode>
