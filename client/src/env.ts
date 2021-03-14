6854673165b1b802bc375181b70e4da52aa9484e
erik7z <nguita.erik@gmail.com>
2021-03-14 22:13:08 +0200

    fixed sourcing of env variables to react

--- client/src/env.ts
+++ client/src/env.ts
@@ -1,5 +1,5 @@
+export const HOST_URI = process.env.REACT_APP_HOST_IP_ADDRESS
 export const TOKEN_FIELD = 'token'
-export const HOST_URI = 'http://localhost:4000'
 export const PAGINATION_PAGE_SIZE = 5
 export const PAGINATION_PAGES_VISIBLE = 5
 export const BLANK_AVATAR_URL = '/images/avatar_512.png'
